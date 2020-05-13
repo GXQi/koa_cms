b5e87f25a90104dbe0beff97133ff561f9142ccf
GXQi <15809237114@163.com>
2020-05-13 17:36:03 +0800

    前台页面

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,39 @@ router.get('/', async (ctx) => {
   })
 })
 .get('/news', async (ctx) => {
-  await ctx.render('default/news')
+  var cateList = await DB.find('articlecate', {'pid': '5afa56bb416f21368039b05d', $or: [{"status": '1'}, {'status': 1}]})
+  var pid = ctx.query.pid
+    , page = ctx.query.page || 1
+    , pageSize = 3
+    , articleList
+    , articleNum
+
+  // console.log(cateList)
+  if(pid) {
+    articleList = await DB.find('article', {'pid': pid}, {}, {
+      page,
+      pageSize
+    })
+    articleNum = await DB.count('article', {'pid': pid})
+  } else {
+    // 循环子分类获取子分类下面的所有内容
+    var subCateArr = []
+    for(var i = 0; i < cateList.length; i++) {
+      subCateArr.push(cateList[i]._id.toString())
+    }
+    articleList = await DB.find('article', {'pid': {$in: subCateArr}}, {}, {
+      page,
+      pageSize
+    })
+    articleNum = await DB.count('article', {'pid': {$in: subCateArr}})
+  }
+  await ctx.render('default/news', {
+    cateList: cateList,
+    articleList: articleList,
+    pid: pid,
+    page: page,
+    totalPages: Math.ceil(articleNum/pageSize)
+  })
 })
 .get('/service', async (ctx) => {
   var serviceList = await DB.find('article', {'pid': '5ab34b61c1348e1148e9b8c2', $or: [{"status": '1'}, {'status': 1}]})
@@ -37,6 +69,13 @@ router.get('/', async (ctx) => {
   // console.log(ctx.params)
   var id = ctx.params.id
   var contentResult = await DB.find('article', {'_id': DB.getObjectID(id)})
+
+  /**
+   * 1. 根据文章获取文章的分类信息
+   * 2. 根据文章的分类信息，去导航表里面查找当前的分类信息的url
+   * 3. 把url赋值给pathname
+   */
+
   await ctx.render('default/content', {
     list: contentResult[0]
   })
@@ -59,7 +98,6 @@ router.get('/', async (ctx) => {
       pageSize
     })
     articleNum = await DB.count('article', {'pid': pid})
-    console.log(articleNum)
   } else {
     // 循环子分类获取子分类下面的所有内容
     var subCateArr = []
